d63b25d36959b1c4f5a72e2f1663c1cb57507922
MakeMe <aleksander.e.popiolek@gmail.com>
2019-01-31 23:00:53 +0100

    Implemented Map based Services Closes #6.

---
+++ pet-clinic-data/src/main/java/com/apwaver/petclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package com.apwaver.petclinic.services.map;
+
+import com.apwaver.petclinic.model.Owner;
+import com.apwaver.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deletById(id);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
