9298c7d91280dc634cbb4e369f46547cbaee6f61
MakeMe <aleksander.e.popiolek@gmail.com>
2019-02-05 09:54:08 +0100

    added PetType service map. closes #29

--- pet-clinic-data/src/main/java/com/apwaver/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/apwaver/petclinic/services/PetService.java
@@ -2,7 +2,6 @@ package com.apwaver.petclinic.services;
 
 import com.apwaver.petclinic.model.Pet;
 
-import java.util.Set;
 
 public interface PetService extends CrudService<Pet,Long>{
 
---
+++ pet-clinic-data/src/main/java/com/apwaver/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.apwaver.petclinic.services;
+
+import com.apwaver.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
