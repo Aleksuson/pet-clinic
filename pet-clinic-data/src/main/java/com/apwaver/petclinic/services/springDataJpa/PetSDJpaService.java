179a62494ec75af2c673f02b9baa95aeb4775793
MakeMe <aleksander.e.popiolek@gmail.com>
2019-02-06 17:24:09 +0100

    added spring data jpa service for Pets. closes #43

---
+++ pet-clinic-data/src/main/java/com/apwaver/petclinic/services/springDataJpa/PetSDJpaService.java
@@ -0,0 +1,46 @@
+package com.apwaver.petclinic.services.springDataJpa;
+
+import com.apwaver.petclinic.model.Pet;
+import com.apwaver.petclinic.repositories.PetRepository;
+import com.apwaver.petclinic.services.PetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+
+        petRepository.findAll().iterator().forEachRemaining(pets::add);
+
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
