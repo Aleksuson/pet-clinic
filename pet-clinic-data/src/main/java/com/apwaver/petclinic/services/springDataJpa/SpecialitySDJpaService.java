cd2f3385ae8e13b8b64d93f3f0690368af45fc53
MakeMe <aleksander.e.popiolek@gmail.com>
2019-02-06 17:27:23 +0100

    added spring data jpa service for Specialities. closes #44

---
+++ pet-clinic-data/src/main/java/com/apwaver/petclinic/services/springDataJpa/SpecialitySDJpaService.java
@@ -0,0 +1,46 @@
+package com.apwaver.petclinic.services.springDataJpa;
+
+import com.apwaver.petclinic.model.Specialty;
+import com.apwaver.petclinic.repositories.SpecialtyRepository;
+import com.apwaver.petclinic.services.SpecialityService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SpecialitySDJpaService implements SpecialityService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialitySDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties = new HashSet<>();
+
+        specialtyRepository.findAll().iterator().forEachRemaining(specialties::add);
+
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
