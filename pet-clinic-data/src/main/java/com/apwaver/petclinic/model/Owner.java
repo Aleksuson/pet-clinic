159df7ed288e82f1a3fceb0028fc25809de58d87
MakeMe <aleksander.e.popiolek@gmail.com>
2019-02-07 19:44:13 +0100

    Upgraded to JUnit5 #4

--- pet-clinic-data/src/main/java/com/apwaver/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/apwaver/petclinic/model/Owner.java
@@ -10,14 +10,13 @@ import java.util.Set;
 @Setter
 @Getter
 @NoArgsConstructor
-@Builder
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
 
     @Builder
-    public Owner(Long id, String firtsName, String lastName, String address, String city, String telephone, Set<Pet> pets) {
-        super(id, firtsName, lastName);
+    public Owner(Long id, String firtsname, String lastName, String address, String city, String telephone, Set<Pet> pets) {
+        super(id, firtsname, lastName);
         this.address = address;
         this.city = city;
         this.telephone = telephone;
